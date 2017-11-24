56b0f58f3d2a204184e6237236892a177ad771df
psyrc5 <psyrc5@nottingham.ac.uk>
2017-11-24 21:02:19 +0000

    modified task1b.c

--- task1b.c
+++ task1b.c
@@ -3,6 +3,18 @@
 #include <stdio.h>
 #include <sys/time.h>
 
+struct process *head = NULL;
+struct process *next = NULL;
+struct process *tail = NULL;
+ 
+long int responsetime = 0;
+int avgresponsetime = 0;
+long int turnaroundtime = 0;
+int avgturnaroundtime =0;
+int prevburst = 0;
+int newburst = 0;
+int state = 0;
+
 void add(struct process **head, struct process *next, struct process **tail) {
     if (*head == NULL) {
         *head = next;
@@ -29,40 +41,15 @@ struct process *returnhead(struct process **head) {
     return temp;
 }
 
-int main(void) {
-    struct process *head = NULL;
-    struct process *next = NULL;
-    struct process *tail = NULL;
-    struct timeval oTimeEnd;
-    long int responsetime = 0;
-    int avgresponsetime = 0;
-    long int turnaroundtime = 0;
-    int avgturnaroundtime =0;
-    int prevburst = 0;
-    int newburst = 0;
-    //if 0 - it hasn't run before, 1 - it has tun before
-    int didyourun[NUMBER_OF_PROCESSES];
-
-    for(int n = 0; n<NUMBER_OF_PROCESSES; n++) {
-        didyourun[n] = 0;
-        next = generateProcess();
-        add(&head,next,&tail);  
-    }
-    
-    while(head != NULL) {
-        prevburst = head->iBurstTime;
-        simulateRoundRobinProcess(head, &(head->oTimeCreated), &(oTimeEnd));
-        newburst = head-> iBurstTime;
-        turnaroundtime += getDifferenceInMilliSeconds(head->oTimeCreated, oTimeEnd);
-        responsetime = turnaroundtime -(prevburst-newburst);
-        if (head->iState != FINISHED) {
-            didyourun[head->iProcessId]=1;
+void print() {
+    if (head->iState != FINISHED) {
             avgresponsetime+=responsetime;
             printf("Process Id = %d, Previous Burst Time = %d, New Burst Time = %d, Response time = %ld\n", head->iProcessId, prevburst, newburst, responsetime);
             next = returnhead(&head);
             add(&head, next, &tail);
 
-        } else if(didyourun[head->iProcessId] == 0){
+        } else if(state == 1){
+            state --;
             avgresponsetime +=responsetime;
             avgturnaroundtime += turnaroundtime;
             printf("Process Id = %d, Previous Burst Time = %d, New Burst Time = %d, Response time = %ld, Turn Around Time = %ld \n", head->iProcessId, prevburst, newburst, responsetime, turnaroundtime);
@@ -74,7 +61,29 @@ int main(void) {
             printf("Process Id = %d, Previous Burst Time = %d, New Burst Time = %d, Turn Around Time = %ld \n", head->iProcessId, prevburst, newburst, turnaroundtime);
             freememory(&head);
     }
+}
+
+int main(void) {
+    struct timeval oTimeEnd;
+
+    for(int n = 0; n<NUMBER_OF_PROCESSES; n++) {
+        next = generateProcess();
+        add(&head,next,&tail);  
     }
+    
+    while(head != NULL) {
+
+        if(head->iState == NEW){
+            state = 1;
+        }
+        prevburst = head->iBurstTime;
+        simulateRoundRobinProcess(head, &(head->oTimeCreated), &(oTimeEnd));
+        newburst = head-> iBurstTime;
+        turnaroundtime += getDifferenceInMilliSeconds(head->oTimeCreated, oTimeEnd);
+        responsetime = turnaroundtime -(prevburst-newburst);
+        print();
+    }
+
     printf("Average response time  = %f \nAverage turnaround time = %f\n",(1.0* avgresponsetime/NUMBER_OF_PROCESSES),(1.0*avgturnaroundtime/NUMBER_OF_PROCESSES));
     exit (EXIT_SUCCESS);
 }
